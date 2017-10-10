e064f9d069262e5aeff5639c80bbbc62b840d50f
kazu <kazu8160@gmail.com>
2017-10-10 22:12:26 +0900

    false -> null

--- src/Dialog.tsx
+++ src/Dialog.tsx
@@ -8,8 +8,8 @@ interface IProps {
 
 const root = document.getElementById('dialog');
 
-const Dialog: React.SFC<IProps> = ({ isShow, closeDialog }: IProps) => {
-  if (!isShow) return false;
+const Dialog: React.SFC<IProps> = ({ isShow, closeDialog }: IProps): JSX.Element | null => {
+  if (!isShow) return null;
 
   const component = [
     <div key="overlay" className="overlay" onClick={() => closeDialog()}></div>,
